e90f27fb03f06e73f0960103d8658c44d62a1d12
Edzer Dionido <jeddionido26@gmail.com>
2024-07-08 18:57:29 +0800

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home.homepage');
+Route::get('signup', [UserController::class, 'signup'])->name('user.signup');
+Route::get('login', [UserController::class, 'login'])->name('user.login');
+
+
+// USER
+Route::resource('user', UserController::class)->except(['index', 'create']);
+Route::post('login', [UserController::class, 'loginUser'])->name('user.loginUser');
