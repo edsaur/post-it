6e0257093739c485cbace866d9dbefccc37bf2cb
Edzer Dionido <jeddionido26@gmail.com>
2024-07-12 16:41:37 +0800

    fix the placements of flash messages

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -1,3 +1,20 @@
-<x-layout $title='Dashboard'>
-    
+<x-layout title='Dashboard'>
+    @if (session('success'))
+        <p>{{session('success')}}</p>
+    @endif
+    <a href="{{route('post.create')}}" class="btn btn-primary">Create Task</a>
+    @if ($posts->isNotEmpty())
+        @if(session('error')) 
+            <p>{{session('error')}}</p>
+        @endif
+
+    @foreach ($posts as $post)
+        <p><a href="{{route('post.show', $post->id)}}">{{ $post->title }} by {{ $post->user->username }}</a></p>
+    @endforeach
+@else 
+    @if(session('error')) 
+        <p>{{session('error')}}</p>     
+    @endif
+    <h1>No Tasks Yet!</h1>
+@endif
 </x-layout>
